566c51de5789f4f405c0d9710e1cc58db5005621
PutoPug <62641095+PutoPug@users.noreply.github.com>
2021-04-15 10:36:28 +0530

    1.0.4

--- README.md
+++ README.md
@@ -3,6 +3,8 @@ Random death messages in minecraft!
 
 # Code for old versions!
 
+[1.0.3-1.16.X](https://github.com/PugsMods/RandomDeathMessage/tree/8a6c2e35167fa55e09de05c6867d5f921262f8b7)
+
 [1.0.2-1.16.X](https://github.com/PugsMods/RandomDeathMessage/tree/701285f815638c5e4211a7cbad2283c179ccd6af)
 
 [1.0.1-1.16.X](https://github.com/PugsMods/RandomDeathMessage/tree/f8f5e9dd1f4a159383f1ba8ed4ae4f0f7fd8ecf2)
--- src/main/java/com/putopug/randomdeathmessage/mixin/FallOutOfWorld.java
+++ src/main/java/com/putopug/randomdeathmessage/mixin/FallOutOfWorld.java
@@ -20,7 +20,7 @@ public class FallOutOfWorld {
     @Overwrite
     public ITextComponent getLocalizedDeathMessage(LivingEntity p_151519_1_) {
         Random index = new Random();
-        String[] arr = {"wasted","game_over","ditch","fail","stop_trying","oof","throw","eject","killed","gravity","huston","flight","cooked","stupid","med","404","high_sugar","f","kick","timeout","drill_forgot","no_breath","tp500","packet_fail","eol","tech","rmdir","taskkill","player_hate_world","terminator","lion","no_appointment","busted","thanos_snap","soft","aim"};
+        String[] arr = {"wasted","game_over","ditch","fail","stop_trying","oof","throw","eject","killed","gravity","huston","flight","cooked","stupid","med","404","high_sugar","f","kick","timeout","drill_forgot","no_breath","tp500","packet_fail","eol","tech","rmdir","taskkill","player_hate_world","terminator","lion","no_appointment","busted","thanos_snap","soft","aim","homework","jawdrop","lag","all_make_mistakes","hmm","did_i_mention","no_gravity","balloon","error","sigh"};
         String s = "death.rdm.";
         String s1 = s + arr[index.nextInt(arr.length)];
         return new TranslationTextComponent(s1, p_151519_1_.getDisplayName());
