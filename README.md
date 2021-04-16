a95fb7e4a88f25ad26bace94e7998bbecbd53793
PutoPug <62641095+PutoPug@users.noreply.github.com>
2021-04-15 23:20:38 -0500

    1.0.5

--- README.md
+++ README.md
@@ -3,6 +3,8 @@ Random death messages in minecraft!
 
 # Code for old versions!
 
+[1.0.4-1.16.X](https://github.com/PugsMods/RandomDeathMessage/tree/566c51de5789f4f405c0d9710e1cc58db5005621)
+
 [1.0.3-1.16.X](https://github.com/PugsMods/RandomDeathMessage/tree/8a6c2e35167fa55e09de05c6867d5f921262f8b7)
 
 [1.0.2-1.16.X](https://github.com/PugsMods/RandomDeathMessage/tree/701285f815638c5e4211a7cbad2283c179ccd6af)
--- src/main/java/com/putopug/randomdeathmessage/mixin/FallDamage.java
+++ src/main/java/com/putopug/randomdeathmessage/mixin/FallDamage.java
@@ -16,5 +16,5 @@ import java.util.Random;
 @Mixin(ServerPlayerEntity.class)
 @SuppressWarnings("all")
 public class FallDamage {
-
+    String[] arr = {"wasted","game_over","ditch","fail","stop_trying","oof","throw","eject","killed","gravity","huston","flight","cooked","stupid","med","404","high_sugar","f","kick","timeout","drill_forgot","no_breath","tp500","packet_fail","eol","tech","rmdir","taskkill","player_hate_world","terminator","lion","no_appointment","busted","thanos_snap","soft","aim","homework","jawdrop","lag","all_make_mistakes","hmm","did_i_mention","no_gravity","balloon","error","sigh","stone","princess","twice","no_friends","clumsy","god","detonator","destroyer3001","dumme","sethealth"};
 }
